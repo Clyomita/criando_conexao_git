b3ff42be87c4019cc1db933e43343b77affa69ea
Clyomita <luchesilc@usp.br>
2023-04-28 17:02:53 -0300

    cria arquivo com passo a passo da conexão com o git

--- conexaogit.R
+++ conexaogit.R
@@ -1,9 +1,16 @@
+#setup da conexão com seu perfil no github
 usethis::use_git_config(user.name = "Clyomita", 
                                                   user.email = "luchesilc@usp.br")
+usethis::use_git()
+#cria novo token
 usethis::create_github_token()
+
  #opção 2 conexao com git
 gitcreds::gitcreds_set()
-usethis::use_git()  
+
+#modificando o token
+#control+enter e cole o novo token (entre aspas) no Renviron, salva o arquivo 
 usethis::edit_r_environ()
-usethis::git_sitrep
+
+#verificando se está tudo certo
 usethis::use_github()
