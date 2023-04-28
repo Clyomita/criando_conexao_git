7405c47c4f6be66b0a117acf0907d835addac4e2
Clyomita <luchesilc@usp.br>
2023-04-28 16:57:18 -0300

    Initial commit

--- conexaogit.R
+++ conexaogit.R
@@ -2,7 +2,7 @@ usethis::use_git_config(user.name = "Clyomita",
                                                   user.email = "luchesilc@usp.br")
 usethis::create_github_token()
  #opção 2 conexao com git
-  gitcreds::gitcreds_set()
+gitcreds::gitcreds_set()
 usethis::use_git()  
 usethis::edit_r_environ()
 usethis::git_sitrep
