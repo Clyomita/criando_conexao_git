c10447e18ffa101fb63a4dcd367f627024afd634
Clyomita <luchesilc@usp.br>
2023-04-28 16:30:50 -0300

    git rep

---
+++ conexaogit.R
@@ -0,0 +1,10 @@
+usethis::use_git_config(user.name = "Clyomita", 
+                                                  user.email = "luchesilc@usp.br")
+usethis::create_github_token()
+ #opção 2 conexao com git
+  gitcreds::gitcreds_set()
+usethis::use_git()  
+2
+3
+usethis::git_sitrep
+usethis::use_github()
